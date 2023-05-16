ada1f6cdfd674ae6a78c073d49dba1562e52070a
Naylimar Alvarez <naylimar27@gmail.com>
2023-05-16 11:33:10 -0400

    añadiendo en objeto cipher propiedades encode y decode y modificando index.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,28 @@
 const cipher = {
-  // ...
+  encode: function (offset, string) {
+    let encodedString = "";
+    for (let i = 0; i < string.length; i++) {
+      let char = string[i];
+      const code = string.charCodeAt(i);
+      if (code >= 32 && code <= 126) {
+        char = String.fromCharCode(((code - 32 + offset) % 95) + 32);
+      }
+      encodedString += char;
+    } return encodedString;
+  },
+
+  decode: function (offset, string) {
+    let decodedString = "";
+    for (let i = 0; i < string.length; i++) {
+      let char = string[i];
+      const code = string.charCodeAt(i);
+      if (code >= 32 && code <= 126) {
+        char = String.fromCharCode(((code - 32 - offset + 95) % 95) + 32);
+      }
+      decodedString += char;
+    }
+    return decodedString;
+  }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,52 +1,27 @@
+import cipher from './cipher.js';
 
+const btnEncode = document.getElementById('btn1');
+const btnDecode = document.getElementById('btn2');
+const inputMessage = document.getElementById('clave');
+const rangeOffset = document.getElementById('rango');
+const result = document.getElementById('encriptado')
 
+console.log('escuchando');
 
-const alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-
-
-function cifrar() {
-
-  const clave = document.getElementById('clave');
-  const rango = document.getElementById('rango');
-  const claveEncriptada = document.getElementById('encriptado');
-  const palabra = clave.value.toUpperCase();
-  let palabraCifrada = "";
-  for (let i = 0; i < palabra.length; i++) {
-    const charSinCodificar = palabra[i];
-    const indexChar = alfabeto.indexOf(charSinCodificar);
-
-    if (indexChar === -1) {
-      palabraCifrada += charSinCodificar;
-    } else {
-      const indexCharCifrado = (indexChar + parseInt(rango.value)) % alfabeto.length;
-      palabraCifrada += alfabeto[indexCharCifrado];
-    }
-  }
-
-  claveEncriptada.value = palabraCifrada;
-}
-
-
-function descifrar() {
- 
-  const clave = document.getElementById('clave');
-  const rango = document.getElementById('rango');
-  const claveDesencriptada = document.getElementById('encriptado');
-  const palabra = clave.value.toUpperCase();
-  let palabraDescifrada = "";
+btnEncode.addEventListener("click", (e) => {
+  e.preventDefault();
+  const string = inputMessage.value;
+  const offset = parseInt(rangeOffset.value);
+  const encodedMessage = cipher.encode(offset, string);
   
-  for (let i = 0; i < palabra.length; i++) {
-    const charCodificado = palabra[i];
-    const indexChar = alfabeto.indexOf(charCodificado);
+  result.value = encodedMessage;
+});
 
-    if (indexChar === -1) {
-      palabraDescifrada += charCodificado;
-    } else {
-      const indexCharDescifrado = (indexChar - parseInt(rango.value) + alfabeto.length) % alfabeto.length;
-      palabraDescifrada += alfabeto[indexCharDescifrado];
-    }
-  }
+btnDecode.addEventListener("click", (e) => {
+  e.preventDefault();
+  const string = inputMessage.value;
+  const offset = parseInt(rangeOffset.value);
+  const decodedMessage = cipher.decode(offset, string);
 
-  claveDesencriptada.value = palabraDescifrada;
-}
+  result.value = decodedMessage;
+});
